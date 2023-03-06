ecbbedf9dc60384de6cc344e6fed98301735373a
Dmirr <dmitriyrodionow@gmail.com>
2023-03-06 13:55:58 +0100

    modified spec

--- cypress/e2e/My 1st cy tests/spec1.cy.js
+++ cypress/e2e/My 1st cy tests/spec1.cy.js
@@ -2,9 +2,6 @@ describe("1-st cypress test spec", () => {
   beforeEach(() => {
     cy.visit("https://staging.lpitko.ru");
   });
-  it("passes", () => {
-    cy.visit("https://staging.lpitko.ru");
-  });
   it("displays text", () => {
     cy.get(
       "#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > span"
