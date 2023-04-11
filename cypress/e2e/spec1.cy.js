f124b727cc0811824636f1893b95cc3dd22c44b8
Dmirr <dmitriyrodionow@gmail.com>
2023-04-11 17:29:03 +0200

    22

--- cypress/e2e/spec1.cy.js
+++ cypress/e2e/spec1.cy.js
@@ -1,9 +1,42 @@
-it("Test for then", () => {
+beforeEach(() => {
   cy.visit("https://example.cypress.io/todo");
-  cy.get('[data-test="new-todo"]').type("test{enter}");
-  cy.get('[data-test="new-todo"]').type("test{enter}");
-  cy.get('[data-test="new-todo"]').type("test{enter}");
-  cy.get(".todo-list li").then((number) => {
-    cy.log(JSON.stringify(number));
+});
+describe("1-st cypress test spec", () => {
+  before(() => {
+    cy.log("runs once before all tests");
+  });
+
+  after(() => {
+    cy.log("runs once after all tests in the block");
+  });
+
+  it("Test for then", () => {
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get(".todo-list li").then((number) => {
+      //cy.log(JSON.stringify(number)); выводим строковое значение
+      //cy.log(number.lengh)
+      //expect(number.length).to.equal(5);
+      assert.equal(number.length, 5, "Array number is");
+    });
+  });
+});
+
+describe("1-st cypress test spec", () => {
+  before(() => {
+    cy.log("2 runs once before all tests");
+  });
+
+  it("Test for then", () => {
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get('[data-test="new-todo"]').type("test{enter}");
+    cy.get(".todo-list li").then((number) => {
+      //cy.log(JSON.stringify(number)); выводим строковое значение
+      cy.log(number.lengh);
+      //expect(number.length).to.equal(5);
+      //assert.equal(number.length, 5, "Array number is");
+    });
   });
 });
